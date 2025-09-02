fd4e4e02a54dd9a185308a81ca90b9d6ddf0bd1e
Ascendion <Ascendion@ASCENDI-9FUK43K>
2025-09-02 09:29:51 +0530

    Add project files.

---
+++ MyProj1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyProj1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
